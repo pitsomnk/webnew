724c2050cf7f87d187d3207df5a757bd67922030
pitsomnk <pitsomnk@gmail.com>
2024-11-18 13:59:38 +0200

    update

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,8 +6,8 @@ const Guide = () => {
     <section className="flexCenter flex-col">
       <div className="padding-container max-container w-full pb-24">
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Your Guide You to a memorable experience</h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">In search of the perfect getaway? You've found it! Our unique blend of comfort and adventure promises an unforgettable experience. Located in a serene neighborhood, our welcoming home is ideal for a relaxing escape or an adventurous journey. Explore our on-site art gallery and be inspired by the beautiful collections on display.</p>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Your Guide to a unique experience</h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Your adventure starts now! We combine comfort and thrill to give you an unforgettable stay. Tucked away in a tranquil neighborhood, our cozy spot is ideal for a relaxing retreat or a lively escapade. Experience our exclusive art gallery, filled with mesmerizing works from the lucky sibiya studio!</p>
         </div>
       </div>
 
